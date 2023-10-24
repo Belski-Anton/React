ce271b9f0fbaf66414f90a50a65c7527b8816616
Anton Belski <anton.belski@gmail.com>
2023-10-24 18:14:15 +0300

    fix: change eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,28 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true , node:true},
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    root: true,
+    env: { browser: true, es2020: true, node: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
     ],
-  },
+    plugins: ['react', '@typescript-eslint', 'prettier'],
+    rules: {
+        'prettier/prettier': 'error',
+        'react/react-in-jsx-scope': 'off',
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
 }
