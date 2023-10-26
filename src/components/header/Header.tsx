9e80e50176b19905e09428bf062440e8a2e0349e
Anton Belski <anton.belski@gmail.com>
2023-10-26 17:38:48 +0300

    feat: add style body and header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import './Header.css'
 import arrow from '../../assets/icon/arrow-down.png'
+import title from '../../assets/img/title-star-wars.png'
 class Header extends React.Component {
     render() {
         return (
             <header>
-                <h1>Star Wars</h1>
+                <h1>
+                    <img className="imgTitle" src={title} alt="name" />
+                </h1>
                 <img src={arrow} alt="arrow" className="arrow" />
-                <h2 className="text-flicker">ENTER NAME</h2>
+                <h2 className="text-flicker">Star Wars Heroes</h2>
                 <img src={arrow} alt="arrow" className="arrow" />
             </header>
         )
