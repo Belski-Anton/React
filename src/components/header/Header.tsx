982d2d171bced439c10f43e1e5dd25007f2b2d47
Anton Belski <anton.belski@gmail.com>
2023-10-26 15:17:36 +0300

    feat: add img to header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import './Header.css'
+import arrow from '../../assets/icon/arrow-down.png'
 class Header extends React.Component {
     render() {
-        return <h1>Star Wars</h1>
+        return (
+            <header>
+                <h1>Star Wars</h1>
+                <img src={arrow} alt="arrow" className="arrow" />
+                <h2 className="text-flicker">ENTER NAME</h2>
+                <img src={arrow} alt="arrow" className="arrow" />
+            </header>
+        )
     }
 }
 
