563df1c11ad7a4500ab5c55f6023d1967a03ddda
Anton Belski <anton.belski@gmail.com>
2023-10-27 12:06:35 +0300

    feat: add fetch and font

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,15 +1,76 @@
 import React from 'react'
 import './Main.css'
-class Main extends React.Component {
+
+interface Person {
+    name: string
+    height: string
+    mass: string
+    hair_color: string
+    skin_color: string
+    eye_color: string
+    birth_year: string
+    gender: string
+}
+
+interface MainProps {}
+
+interface MainState {
+    error: Error | null
+    isLoaded: boolean
+    items: Person[]
+}
+
+class Main extends React.Component<MainProps, MainState> {
+    constructor(props: MainProps) {
+        super(props)
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: [],
+        }
+    }
+
+    componentDidMount(): void {
+        fetch('https://swapi.dev/api/people/')
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        items: result.results,
+                    })
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error,
+                    })
+                }
+            )
+    }
+
     render() {
-        const cards = Array(12).fill(null)
-        return (
-            <div className="wrapperMain">
-                {cards.map((_, index) => (
-                    <div key={index} className="card"></div>
-                ))}
-            </div>
-        )
+        const { items, isLoaded, error } = this.state
+
+        if (error) {
+            return <div>Error: {error.message}</div>
+        } else if (!isLoaded) {
+            return <div>Loading...</div>
+        } else {
+            return (
+                <div className="wrapperMain">
+                    {items.map((item, index) => (
+                        <div key={index} className="card">
+                            <p>Name: {item.name}</p>
+                            <p>Height: {item.height}</p>
+                            <p>Mass: {item.mass}</p>
+                            <p>Birth Year: {item.birth_year}</p>
+                            <p>Gender: {item.gender}</p>
+                        </div>
+                    ))}
+                </div>
+            )
+        }
     }
 }
 
