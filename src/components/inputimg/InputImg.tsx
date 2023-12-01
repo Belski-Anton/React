e50f6b6fc91c45c22017d838bdf8388c44a8df2a
Anton Belski <anton.belski@gmail.com>
2023-12-01 13:45:23 +0300

    feat: add useRef to input file

--- src/components/inputimg/InputImg.tsx
+++ src/components/inputimg/InputImg.tsx
@@ -1,11 +1,42 @@
+import { useRef } from 'react'
 import './InputImg.css'
+
 const InputImg = () => {
+    const bodyInputRef = useRef<HTMLInputElement | null>(null)
+    const imgRef = useRef<HTMLImageElement | null>(null)
+
+    const handleCnange = () => {
+        const inputElement = bodyInputRef.current
+        if (inputElement && inputElement.files && inputElement.files[0]) {
+            const selectedFile = inputElement.files[0]
+            convertBase64(selectedFile)
+        }
+    }
+
+    const convertBase64 = (file: File) => {
+        const reader = new FileReader()
+        reader.onload = (e) => {
+            const base64Data = e.target?.result as string
+            if (imgRef.current) {
+                imgRef.current.src = base64Data
+            }
+        }
+        reader.readAsDataURL(file)
+    }
+
     return (
-        <div>
+        <div className="img_wrapper">
             <label htmlFor="file-upload" className="custom-file-upload">
-                Download
+                Загрузить
             </label>
-            <input id="file-upload" type="file" />
+            <input
+                onChange={handleCnange}
+                ref={bodyInputRef}
+                id="file-upload"
+                type="file"
+                accept="image/png, image/jpeg"
+            />
+            <img ref={imgRef} alt="" />
         </div>
     )
 }
