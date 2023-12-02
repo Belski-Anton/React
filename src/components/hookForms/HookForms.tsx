71e0225d55cb95c62287b6229d75e30110bd1b9d
Anton Belski <anton.belski@gmail.com>
2023-12-02 23:12:34 +0300

    feat: add  input file to redux use hook form

--- src/components/hookForms/HookForms.tsx
+++ src/components/hookForms/HookForms.tsx
@@ -1,7 +1,10 @@
 import { useDispatch } from 'react-redux'
 import { IForm, formActions } from '../../store/formSlice'
-import './HookForms.css'
 import { useForm, SubmitHandler } from 'react-hook-form'
+import { useState } from 'react'
+import './HookForms.css'
+import '../inputimg/InputImg.css'
+import '../radiobutton/RadioButton.css'
 
 const HookForms = () => {
     const { register, handleSubmit } = useForm<IForm>({
@@ -9,8 +12,11 @@ const HookForms = () => {
     })
     const dispatch = useDispatch()
     const onSubmit: SubmitHandler<IForm> = (data: IForm) => {
-        dispatch(formActions.addForm(data))
+        const formData = { ...data, image: imageBase64 }
+        dispatch(formActions.addForm(formData))
     }
+    const [imageBase64, setImageBase64] = useState('')
+
     return (
         <div className="wrapper_form">
             <div className="form">
@@ -51,9 +57,74 @@ const HookForms = () => {
                         placeholder="Enter your age... "
                         className="input"
                     />
-                    <div className="btn ">
-                        <button className="button">Submit</button>
+                    <input
+                        type="text"
+                        {...register('country')}
+                        placeholder="Country"
+                        className="input"
+                    />
+                    <div className="radio_button">
+                        <input
+                            type="radio"
+                            value="male"
+                            {...register('gender')}
+                        />
+                        <label htmlFor="male">male</label>
+                        <input
+                            type="radio"
+                            value="female"
+                            {...register('gender')}
+                        />
+                        <label htmlFor="female">female</label>
                     </div>
+
+                    <div className="img_wrapper">
+                        <label
+                            htmlFor="file-upload"
+                            className="custom-file-upload background_color"
+                        >
+                            Downland img
+                        </label>
+                        <input
+                            {...register('image')}
+                            id="file-upload"
+                            type="file"
+                            accept="image/png, image/jpeg"
+                            onChange={(
+                                e: React.ChangeEvent<HTMLInputElement>
+                            ) => {
+                                const file = e.target.files && e.target.files[0]
+                                if (file) {
+                                    const maxSizeInBytes = 5 * 1024 * 1024
+                                    if (file.size <= maxSizeInBytes) {
+                                        const reader = new FileReader()
+                                        reader.onload = (event) => {
+                                            setImageBase64(
+                                                event.target?.result as string
+                                            )
+                                        }
+                                        reader.readAsDataURL(file)
+                                    } else {
+                                        alert(
+                                            'File size exceeds the maximum allowed size.'
+                                        )
+                                    }
+                                }
+                            }}
+                        />
+                        <img src={imageBase64} className="img" alt="" />
+                    </div>
+                    <div>
+                        <input
+                            type="checkbox"
+                            id="scales"
+                            {...register('isAgree')}
+                        />
+                        <label htmlFor="scales">I agree</label>
+                    </div>
+                    <button className="button btn_center" type="submit">
+                        Submit
+                    </button>
                 </form>
             </div>
         </div>
