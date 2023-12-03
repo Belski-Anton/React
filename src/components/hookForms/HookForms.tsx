76110ba50632ecb1cf00e835dda979074bb889ed
Anton Belski <anton.belski@gmail.com>
2023-12-03 22:01:14 +0300

    feat: add validation input to hook form

--- src/components/hookForms/HookForms.tsx
+++ src/components/hookForms/HookForms.tsx
@@ -7,6 +7,7 @@ import * as yup from 'yup'
 import './HookForms.css'
 import '../inputimg/InputImg.css'
 import '../radiobutton/RadioButton.css'
+import { useNavigate } from 'react-router-dom'
 
 const schema = yup
     .object({
@@ -22,7 +23,11 @@ const schema = yup
             .string()
             .oneOf([yup.ref('password')], 'Passwords must match')
             .required('Confirm password is required'),
-        name: yup.string().required('Name is required'),
+        name: yup
+            .string()
+            .matches(/^[A-ZА-Я]/, 'Name must start with a capital letter')
+            .required('Name is required'),
+
         age: yup
             .number()
             .positive('Age must be a positive number')
@@ -48,10 +53,12 @@ const HookForms = () => {
         defaultValues: {},
     })
     const dispatch = useDispatch()
+    const navigate = useNavigate()
 
     const onSubmit: SubmitHandler<IForm> = (data: IForm) => {
         const formData = { ...data, image: imageBase64 }
         dispatch(formActions.addForm(formData))
+        navigate(`../`)
     }
 
     const [imageBase64, setImageBase64] = useState('')
@@ -91,6 +98,40 @@ const HookForms = () => {
         )
     }
 
+    const [passwordStrength, setPasswordStrength] = useState('')
+
+    const calculatePasswordStrength = (password: string) => {
+        let strength = 0
+        if (password.length > 6) strength += 1
+        if (password.length > 10) strength += 1
+        if (/[A-Z]/.test(password)) strength += 1
+        if (/[a-z]/.test(password)) strength += 1
+        if (/[0-9]/.test(password)) strength += 1
+        if (/[^A-Za-z0-9]/.test(password)) strength += 1
+
+        switch (strength) {
+            case 0:
+            case 1:
+            case 2:
+                return 'Слабый'
+            case 3:
+            case 4:
+                return 'Средний'
+            case 5:
+            case 6:
+                return 'Сильный'
+            default:
+                return ''
+        }
+    }
+
+    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const newPassword = e.target.value
+        setValue('password', newPassword, { shouldValidate: true })
+        const strength = calculatePasswordStrength(newPassword)
+        setPasswordStrength(strength)
+    }
+
     return (
         <div className="wrapper_form">
             <div className="form">
@@ -100,38 +141,104 @@ const HookForms = () => {
                     onSubmit={handleSubmit(onSubmit)}
                     className="wrapper_input"
                 >
-                    <input
-                        type="email"
-                        {...register('email')}
-                        placeholder="Enter your email ..."
-                        className="input"
-                    />
-                    <input
-                        type="password"
-                        {...register('password')}
-                        placeholder="Enter your password..."
-                        className="input"
-                    />
-                    <input
-                        type="password"
-                        {...register('confirmPassword')}
-                        placeholder="Confirm the password..."
-                        className="input"
-                    />
-
-                    <input
-                        type="text"
-                        {...register('name')}
-                        placeholder="Enter your name... "
-                        className="input"
-                    />
-                    <input
-                        type="text"
-                        {...register('age')}
-                        placeholder="Enter your age... "
-                        className="input"
-                    />
-                    <p>{errors.age?.message}</p>
+                    <div className="wrapper_input">
+                        <input
+                            type="email"
+                            {...register('email')}
+                            placeholder="Enter your email ..."
+                            className="input"
+                        />
+                        <span
+                            style={{
+                                fontSize: '10px',
+                                color: 'red',
+                                position: 'absolute',
+                                top: '75px',
+                            }}
+                        >
+                            {errors.email?.message}
+                        </span>
+                    </div>
+                    <div className="wrapper_input">
+                        <input
+                            type="password"
+                            {...register('password')}
+                            placeholder="Enter your password..."
+                            className="input"
+                            onChange={handlePasswordChange}
+                        />
+                        <span
+                            style={{
+                                fontSize: '10px',
+                                color: 'red',
+                                position: 'absolute',
+                                top: '75px',
+                            }}
+                        >
+                            {errors.password?.message}
+                        </span>
+                        <div
+                            className={`password-strength-indicator ${passwordStrength}`}
+                        >
+                            {passwordStrength && (
+                                <span>{passwordStrength}</span>
+                            )}
+                        </div>
+                    </div>
+                    <div className="wrapper_input">
+                        <input
+                            type="password"
+                            {...register('confirmPassword')}
+                            placeholder="Confirm the password..."
+                            className="input"
+                        />
+                        <span
+                            style={{
+                                fontSize: '10px',
+                                color: 'red',
+                                position: 'absolute',
+                                top: '75px',
+                            }}
+                        >
+                            {errors.confirmPassword?.message}
+                        </span>
+                    </div>
+                    <div className="wrapper_input">
+                        <input
+                            type="text"
+                            {...register('name')}
+                            placeholder="Enter your name... "
+                            className="input"
+                        />
+                        <span
+                            style={{
+                                fontSize: '10px',
+                                color: 'red',
+                                position: 'absolute',
+                                top: '75px',
+                            }}
+                        >
+                            {errors.name?.message}
+                        </span>
+                    </div>
+                    <div className="wrapper_input">
+                        <input
+                            type="text"
+                            {...register('age')}
+                            placeholder="Enter your age... "
+                            className="input"
+                        />
+                        <span
+                            style={{
+                                fontSize: '10px',
+                                color: 'red',
+                                position: 'absolute',
+                                top: '75px',
+                            }}
+                        >
+                            {errors.age?.message}
+                        </span>
+                    </div>
                     <div
                         className="autocomplete"
                         onBlur={handleContainerBlur}
@@ -225,6 +332,7 @@ const HookForms = () => {
                                 }
                             }}
                         />
+                        {errors.image?.message}
                         <img src={imageBase64} className="img" alt="" />
                     </div>
 
